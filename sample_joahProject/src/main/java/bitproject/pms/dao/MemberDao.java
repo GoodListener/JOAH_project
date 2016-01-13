9f6675f025cfac9f8a04193824094528bca65027
김종원 <@192.168.0.55>
2016-01-13 13:45:49 +0900

    로그인로그아웃됨

--- sample_joahProject/src/main/java/bitproject/pms/dao/MemberDao.java
+++ sample_joahProject/src/main/java/bitproject/pms/dao/MemberDao.java
@@ -1,11 +1,15 @@
 package bitproject.pms.dao;
 
+import java.util.Map;
+
 import bitproject.pms.domain.Member;
 
 public interface MemberDao {
 /*  List<Student> selectList(Map<String,Object> paramMap);*/
 
-  int insert(Member student);
+  int insert(Member member);
+  
+  Member login(Map<String,Object> paramMap);
 /*
   int delete(String email);
   
