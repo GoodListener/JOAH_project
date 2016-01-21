28a11d1779fa4a3e4b5f932f12ca913d4b42185a
김종원 <@192.168.0.36>
2016-01-21 14:45:34 +0900

    로그인 관련 자바스크립트

---
+++ sample_joahProject/src/main/webapp/bitproject/js/auth/login_logout.js
@@ -0,0 +1,57 @@
+//로그인 관련 자바 스크립트.
+
+$(document).ready( function() {
+	
+    var id = $.cookie("id");
+    if (id != null && id != "") {
+      $("#signin-userID").val(id); // id textbox에 쿠키값 출력
+      $("#saveId").attr("checked", "checked"); // 체크박스에 체크
+    }
+  });  
+
+    $('#loginBtn').click( function(event) {
+          var checkStatus = $("#saveId").is(":checked");
+          if (checkStatus) {
+            $.cookie("id", $("#signin-userID").val());
+          } else {
+            $.cookie("id", "");
+          }   
+           
+          $.post('ajax/login.do', {
+            id : $('#signin-userID').val(),
+            password : $('#signin-password').val(),
+          }, function(resultObj) {
+            var ajaxResult = resultObj.ajaxResult;
+            if (ajaxResult.status == "success") {
+              idSession = ajaxResult.data.id;
+              sessionStorage.setItem('loginSession',idSession);
+              location.href = "";
+            } else {
+              alert("없는 아이디 이거나 패스워드가 일치하지 않습니다.");
+            }
+          }, 'json');
+        });
+  
+    $('#after_login1').click( function(event) {
+         $.post('ajax/logout.do',
+         function(resultObj) {
+          var ajaxResult = resultObj.ajaxResult;
+          if (ajaxResult.status == "success") {
+            sessionStorage.removeItem('loginSession');
+            location.href = "";
+          }
+        },'json');
+      });
+  
+   if(sessionStorage.getItem('loginSession') != undefined) {
+     $("#after_login1").show();
+     $("#after_login2").show();
+     $("#after_login4").show();
+     $("#after_login2").text(sessionStorage.getItem('loginSession') + "님");
+     $("#after_login3").show();
+     $("#com-content").show();
+  } else {
+    $("#before_login1").show();
+    $("#before_login2").show();
+     $("#com-content2").show();
+  }
